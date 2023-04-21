18458dc728f56181877a6baf369d6647499a8fa9
Louis Bettens <louis@bettens.info>
2023-04-21 15:39:35 +0200

    fix reusable

--- reusable/chal.py
+++ reusable/chal.py
@@ -12,7 +12,7 @@ def encrypt(msg):
 
 flag = "EPFL{1F_Y0U_R3U5E_NONCE5_CL4P_Y0UR_8ANDS}"
 message1 = flag
-message2 = "Lorem Ipsum Dolor Sit Amet"
+message2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 
 print(encrypt(message1).hex())
 print(encrypt(message2).hex())
--- reusable/solve.py
+++ reusable/solve.py
@@ -7,5 +7,5 @@ key = secrets.token_bytes(32)
 
 msg1, msg2 = [bytes.fromhex(m) for m in open("output", 'r').read().splitlines()]
 msg1 = msg1.ljust(len(msg2), b"\0")
-crib = b"Lorem Ipsum Dolor Sit Amet"
+crib = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 print(xor(crib, xor(msg1, msg2)[:len(crib)]))
