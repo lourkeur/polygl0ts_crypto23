2d538d76efcb61adeb2e86f3392c194c62c6b606
Louis Bettens <louis@bettens.info>
2023-04-22 22:19:47 +0200

    add flags

--- reusable/chal.py
+++ reusable/chal.py
@@ -10,7 +10,7 @@ def encrypt(msg):
     ct, tag = cipher.encrypt_and_digest(bytes(msg, "utf8"))
     return ct+tag
 
-flag = "EPFL{1F_Y0U_R3U5E_NONCE5_CL4P_Y0UR_8ANDS}"
+flag = "EPFL{XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX}"
 message1 = flag
 message2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 
